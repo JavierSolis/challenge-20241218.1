7ec34771b390755f0cd7b7c761dd229dc6c5b532
J.Solis <javiersoliscontact@gmail.com>
2024-12-21 04:09:43 -0500

    [ADD] nuevas clases con hijos de Item, distribuyendo responsabilidades segun hijo, los casos de prueba de las clases hijo, nueva clase Conjured

--- game-02/test/test_new.spec.ts
+++ game-02/test/test_new.spec.ts
@@ -1,19 +1,21 @@
-import { Item, GildedRose } from "../app/gilded-rose";
+import { GildedRose } from "../app/gilded-rose-new";
+import * as Models from "../app/gilded-rose-new";
 
 var assert = require('assert');
 
-describe('GildedRose', function () {
-  describe('basics', function () {
+describe('GildedRose NEW', function () {
 
     it('UpdateQuality degrades sellIn 1',function(){
-      const item = {name:"random name", sellIn:3, quality:4} as Item;
+      const item = new Models.NormalItem("random name",3,4);
+    
       const gildedRose = new GildedRose([item]);
       gildedRose.updateQuality();
       assert.equal(gildedRose.items[0].sellIn,2);
     });
 
+    
     it('Once the sell by date has passed, Quality degrades twice as fast',function(){
-      const item = {name:"random name", sellIn:1, quality:4} as Item;
+      const item = new Models.NormalItem("random name",1, 4)
       const gildedRose = new GildedRose([item]);
       gildedRose.updateQuality();
       assert.equal(gildedRose.items[0].sellIn,0,'no disminuyo en 1 la 1ra vez');
@@ -24,54 +26,72 @@ describe('GildedRose', function () {
       assert.equal(gildedRose.items[0].quality,1,'no disminuyo en 2');
     });
 
+    
     it('The Quality of an item is never negative', function () {
-      const item = {name:"random name", sellIn:1, quality:0} as Item;
+      const item = new Models.NormalItem("random name",1, 0);
+      const gildedRose = new GildedRose([ item ]);
+      gildedRose.updateQuality();
+      gildedRose.updateQuality();
+      assert.ok(gildedRose.items[0].quality>=0);
+    });
+  
+    it('The Quality of an item is never negative, extreme case Zero SellIn and One Quality', function () {
+      const item = new Models.NormalItem("random name",0, 1);
       const gildedRose = new GildedRose([ item ]);
       gildedRose.updateQuality();
       gildedRose.updateQuality();
       assert.ok(gildedRose.items[0].quality>=0);
     });
 
-    it('The Quality of an item is never negative, extreme case Zero SellIn and One Quality', function () {
-      const item = {name:"random name", sellIn:0, quality:1} as Item;
+    it('The Quality of an item is never negative, case Conjure', function () {
+      const item = new Models.Conjured("random name",0, 1);
       const gildedRose = new GildedRose([ item ]);
       gildedRose.updateQuality();
       gildedRose.updateQuality();
       assert.ok(gildedRose.items[0].quality>=0);
     });
-    
 
     it('"Aged Brie" actually increases in Quality the older it gets', function () {
-      const item = {name:"Aged Brie", sellIn:3, quality:2} as Item;
+      const item = new Models.AgedBrieItem("Aged Brie",3, 2);
       const gildedRose = new GildedRose([ item ]);
       gildedRose.updateQuality();
       assert.equal(gildedRose.items[0].quality,3);
     })
 
     it('"Aged Brie" actually increases in Quality the older it gets, twice fast', function () {
-      const item = {name:"Aged Brie", sellIn:0, quality:2} as Item;
+      const item = new Models.AgedBrieItem("Aged Brie",0, 2);
       const gildedRose = new GildedRose([ item ]);
       gildedRose.updateQuality();
       assert.equal(gildedRose.items[0].quality,4);
     })
 
     it('The Quality of an item is never more than 50 ( ex Aged Brie )', function () {
-      const item = {name:"Aged Brie", sellIn:0, quality:50} as Item;
+      const item = new Models.AgedBrieItem("Aged Brie",0, 50);
       const gildedRose = new GildedRose([ item ]);
       gildedRose.updateQuality();
       assert.equal(gildedRose.items[0].quality,50);
     })
     
     it('The Quality of an item is never more than 50 ( ex Aged Brie whit negative sellIn )', function () {
-      const item = {name:"Aged Brie", sellIn:-2, quality:49} as Item;
+      const item = new Models.AgedBrieItem("Aged Brie",-2, 49);
       const gildedRose = new GildedRose([ item ]);
       gildedRose.updateQuality();
       assert.equal(gildedRose.items[0].quality,50);
     })
 
+    it('The Quality of an item is never more than 50 ( ex BackstagePasses case limit quality 49 )', function () {
+      const item = new Models.BackstagePasses("Backstage passes to a TAFKAL80ETC concert",10, 49);
+      const gildedRose = new GildedRose([ item ]);
+      gildedRose.updateQuality();
+      gildedRose.updateQuality();
+      assert.equal(gildedRose.items[0].quality,50);
+    })
+
+    
+
     it('"Sulfuras"( failed, changed of code: \'Sulfuras, Hand of Ragnaros\' ), being a legendary item, never has to be sold or decreases in Quality', function () {
       //aqui el test fallo, segun el codigo el nombre es 'Sulfuras, Hand of Ragnaros'
-      const item = {name:"Sulfuras, Hand of Ragnaros", sellIn:1, quality:80} as Item;
+      const item = new Models.Sulfuras("Sulfuras, Hand of Ragnaros",1,80);
       const gildedRose = new GildedRose([ item ]);
       gildedRose.updateQuality();
       assert.equal(gildedRose.items[0].quality,80,'1st attempt');
@@ -79,12 +99,18 @@ describe('GildedRose', function () {
       assert.equal(gildedRose.items[0].quality,80,'2nd attempt');
     })
 
+    it('"Sulfuras" is ever Quality 80', function () {
+      //aqui el test fallo, segun el codigo el nombre es 'Sulfuras, Hand of Ragnaros'
+      const item = new Models.Sulfuras("Sulfuras, Hand of Ragnaros",1,0);
+      assert.equal(item.quality,80);
+    })
+
     describe('"Backstage passes", like aged brie, increases in Quality as its SellIn value approaches',function(){
       //aqui el test fallo, segun el codigo el nombre es
       const nameBackstage = 'Backstage passes to a TAFKAL80ETC concert';
 
       it('Quality increases by 2 when there are beetwen 10 and 5 days (not included) ', function () {
-        const item = {name:nameBackstage, sellIn:10, quality:10} as Item;
+        const item = new Models.BackstagePasses(nameBackstage,10, 10);
         const gildedRose = new GildedRose([ item ]);
         gildedRose.updateQuality();
         assert.equal(gildedRose.items[0].quality,12);
@@ -93,7 +119,7 @@ describe('GildedRose', function () {
       })
 
       it('Quality increases by 3 when there are beetwen 5 and 0 days (included)', function () {
-        const item = {name:nameBackstage, sellIn:5, quality:20} as Item;
+        const item = new Models.BackstagePasses(nameBackstage,5, 20);
         const gildedRose = new GildedRose([ item ]);
         gildedRose.updateQuality();
         assert.equal(gildedRose.items[0].quality,23,'1st attempt');
@@ -102,7 +128,8 @@ describe('GildedRose', function () {
       })
 
       it('Quality drops to 0 after the concert', function () {
-        const item = {name:nameBackstage, sellIn:1, quality:20} as Item;
+        const item = new Models.BackstagePasses(nameBackstage,1, 20);
+        
         const gildedRose = new GildedRose([ item ]);
         gildedRose.updateQuality();
         assert.ok(gildedRose.items[0].quality>0,'1st attempt');
@@ -110,6 +137,6 @@ describe('GildedRose', function () {
         assert.equal(gildedRose.items[0].quality,0,'2nd attempt');
       })
     });
+    
 
-  });
 });
